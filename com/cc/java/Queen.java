08dec84abaf9dc7d5f577615a676aeeb9856716f
Arian <a.hormozfar@yahoo.de>
2024-07-19 11:42:03 +0200

    2. Iteration Test Subklassen

--- com/cc/java/Queen.java
+++ com/cc/java/Queen.java
@@ -2,4 +2,9 @@ package com.cc.java;
 
 public class Queen extends HonnyBee{
 
+    public String doYourJob(){
+        return "I'm a Queen, any questions?";
+    }
+
+
 }
--- com/cc/java/Worker.java
+++ com/cc/java/Worker.java
@@ -2,4 +2,9 @@ package com.cc.java;
 
 public class Worker extends HonnyBee{
 
+    public String doYourJob(){
+        return "I'm a worker bee, I work all day!";
+    }
+    
+
 }
